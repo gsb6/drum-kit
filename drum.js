f3dd05e282c38e9e0ab50a998bf9fa46c5208c26
gabriel brunichaki <gabriel.brunichaki@acad.pucrs.br>
2019-02-20 16:18:31 -0300

    html and javascript splited

---
+++ drum.js
@@ -0,0 +1,17 @@
+const keys = document.querySelectorAll('.key')
+
+window.addEventListener('keydown', e => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add(`playing-${e.keyCode}`);
+    
+})
+
+keys.forEach(key => key.addEventListener('transitionend', e => {
+    if (e.propertyName !== 'transform') return;
+    key.classList.remove(`playing-${key.getAttribute('data-key')}`);
+}))
